e9144c3075458bf114c11a31d1b0f145ca9127ba
min33sky <min33sky@naver.com>
2021-06-21 14:25:41 +0900

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
