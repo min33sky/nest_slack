a58bd99d27d95ccc0cc890d2909f10c36ea06fbd
min33sky <min33sky@naver.com>
2021-06-29 22:40:26 +0900

    start: frontend

---
+++ Backend/README.md
@@ -0,0 +1,87 @@
+# Nest Server
+
+> create nest server
+
+## Install
+
+- npm i
+- npm run schema:sync
+- npm run seed:run
+- npm run start:dev
+
+## Typeorm 관련 참고
+
+1. 기존 DB가 없을 때는 Schema를 직접 생성한 후 `npm run schema:sync` 실행
+2. 기존 DB가 존재할 땐 `npx typeorm-model-generator -h localhost -d 스키마이름 -u DB유저이름 -x 비밀번호 -e 데이터베이스종류(mysql)` 실행
+
+**참고**
+
+- [typeorm-model-generator](https://www.npmjs.com/package/typeorm-model-generator)
+
+## Script Description
+
+- npm run seed:run
+
+  - DB의 sample Data를 넣어주는 script
+
+  - [typeorm-seeding](https://github.com/w3tecch/typeorm-seeding) 모듈을 사용한다.
+
+- npm run schema:drop
+
+  - DB의 Schema를 날려버린다.
+
+- npm run schema:sync
+
+  - DB의 Schema를 생성 및 동기화
+
+- npm run db:create-migration [파일이름]
+
+  - `migration`을 위한 코드를 생성함
+  - migration: 테이블과 엔티티를 수정할 때 사용
+
+- npm run db:generate-migration [파일이름]
+
+  - query를 자동으로 작성해줌. (`Drop`이 있을 경우 위험)
+
+- npm run db:migrate
+
+  - migrate를 실행하는 스크립트
+
+- npm run db:migrate:revert
+
+  - rollback 스크립트
+
+**참고**
+
+- TypeORM은 ts 파일을 인식할 수 없기 때문에 `npm run typeorm` 스크립트를 활용하였다.
+
+## Error Solution
+
+---
+
+### `class-valitator` 사용 시 데코레이터의 대소문자 주의
+
+```js
+// Error
+@isEmail()
+email?: string;
+// Success
+@IsEmail()
+email?: string;
+```
+
+**[참고 링크](https://stackoverflow.com/questions/67045344/unable-to-resolve-signature-of-property-decorator-when-called-as-an-expression)**
+
+- Error: Authentication strategies must have a name
+  - passport-local 대신 passport를 import 했는지 체크
+
+### Cannot add or update a child row: a foreign key constraint fails
+
+- 부모키에 있는 값만 참조할 수 있으니 부모 테이블의 값을 확인해보자
+
+## References
+
+- [Hot Reload](https://docs.nestjs.com/recipes/hot-reload#hot-reload)
+- [Configuration](https://docs.nestjs.com/techniques/configuration#getting-started)
+- [OpenAPI(Swagger)](https://docs.nestjs.com/openapi/introduction)
+- [validation](https://docs.nestjs.com/techniques/validation#transform-payload-objects)
---
+++ Frontend/webpack.config.ts
@@ -0,0 +1,111 @@
+import path from 'path';
+// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import webpack from 'webpack';
+// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+// import WebpackDevServer from 'webpack-dev-server';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+declare module 'webpack' {
+  // eslint-disable-next-line no-unused-vars
+  interface Configuration {
+    // devServer?: WebpackDevServer.Configuration;
+  }
+}
+
+const config: webpack.Configuration = {
+  name: 'sleact',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: !isDevelopment ? 'hidden-source-map' : 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@hooks': path.resolve(__dirname, 'hooks'),
+      '@components': path.resolve(__dirname, 'components'),
+      '@layouts': path.resolve(__dirname, 'layouts'),
+      '@pages': path.resolve(__dirname, 'pages'),
+      '@utils': path.resolve(__dirname, 'utils'),
+      '@typings': path.resolve(__dirname, 'typings'),
+    },
+  },
+  entry: {
+    app: './client',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: { browsers: ['last 2 chrome versions'] },
+                debug: isDevelopment,
+              },
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript',
+          ],
+          env: {
+            // @emotion/babel-plugin, react-hot-loading
+            // development: {
+            //   plugins: ['@emotion', require.resolve('react-refresh/babel')],
+            // },
+            // production: {
+            //   plugins: ['@emotion'],
+            // },
+          },
+        },
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    // ? 타입스크립트 체크와 웹팩이 동시에 돌아가게 설정
+    // new ForkTsCheckerWebpackPlugin({
+    //   async: false,
+    //   // eslint: {
+    //   //   files: "./src/**/*",
+    //   // },
+    // }),
+    // ? process.NODE_ENV를 프론트앤드에서 사용 가능
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: isDevelopment ? 'development' : 'production',
+    }),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/',
+  },
+  // devServer: {
+  //   historyApiFallback: true, // ? react-router (서버는 localhost:3090 주소밖에 모르지만 이 설정을 통해 /xxx 와 같은 주소로 라우팅이 가능하다)
+  //   port: 3090,
+  //   publicPath: '/dist/',
+  //   proxy: {
+  //     '/api/': {
+  //       target: 'http://localhost:3095',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
+};
+
+if (isDevelopment && config.plugins) {
+  // config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // config.plugins.push(new ReactRefreshWebpackPlugin());
+  // config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
+}
+if (!isDevelopment && config.plugins) {
+  // config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+  // config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+}
+
+export default config;
